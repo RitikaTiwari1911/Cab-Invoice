b25b044d7e5c2253201f804f425a476ce9700abc
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-05-10 08:18:49 +0530

    ADDED: Multiple Rides

--- src/main/java/com/invoiceGenerator/InvoiceGenerator.java
+++ src/main/java/com/invoiceGenerator/InvoiceGenerator.java
@@ -1,10 +1,24 @@
 package com.invoiceGenerator;
 
 public class InvoiceGenerator {
+    //constants
     private static final int MINIMUM_COST_PER_KM = 10 ;
     private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
 
+    //method for total fare
     public double calculateFare(double distance, int time) {
-        return  distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+        double totalFare=  distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+        if(totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
+    }
+    //method for multiple rides
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for(Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance,ride.time);
+        }
+        return totalFare;
     }
 }
--- src/test/java/com/invoiceGenerator/InvoiceServiceTest.java
+++ src/test/java/com/invoiceGenerator/InvoiceServiceTest.java
@@ -13,4 +13,23 @@ public class InvoiceServiceTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinimumFare(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(5,fare,0.0);
+    }
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {    new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double totalFare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30,totalFare,0.0);
+    }
+
 }
